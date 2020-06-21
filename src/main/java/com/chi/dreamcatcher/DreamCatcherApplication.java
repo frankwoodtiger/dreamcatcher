69f0e07eaa822e9bd840724e9b2ae8e046bc2e7b
Chi Lee <chi.lee@spxflow.com>
2020-06-21 16:52:48 -0400

    initialized project with Spring Initializr

---
+++ src/main/java/com/chi/dreamcatcher/DreamCatcherApplication.java
@@ -0,0 +1,13 @@
+package com.chi.dreamcatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamCatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamCatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chi/dreamcatcher/DreamCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chi.dreamcatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamCatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
